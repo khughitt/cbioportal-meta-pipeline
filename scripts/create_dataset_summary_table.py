771856ca9e7c4655ee65e591204e24b3c3286632
Keith Hughitt <keith.hughitt@gmail.com>
2025-03-01 20:01:42 -0500

    transpose dataset summary table

--- scripts/create_dataset_summary_table.py
+++ scripts/create_dataset_summary_table.py
@@ -12,4 +12,8 @@ df = pd.concat(counts, axis=1)
 df.columns = [os.path.basename(os.path.dirname(os.path.dirname(x))) for x in snek.input]
 
 df.insert(0, "stat", ['num_genes', 'num_mutations', 'num_cancer_types', 'num_patients', 'num_samples'])
+df = df.set_index('stat').T.reset_index()
+
+df = df.rename(columns={'index': 'dataset'})
+
 df.to_feather(snek.output[0])
